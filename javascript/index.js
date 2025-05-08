a5cb648a5b257199d28ae7873955bf4dcfc90088
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2025-05-08 16:12:17 -0700

    Update with multiple locations and add them dynamically

--- javascript/index.js
+++ javascript/index.js
@@ -11,13 +11,34 @@ losAngelesTimeEl.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]
 
 //PARIS
 let parisEl = document.querySelector("#paris");
-let parisDateEl = parisEl.querySelector(".date");
-let parisTimeEl = parisEl.querySelector(".time");
-let parisTime = moment().tz("Europe/Paris");
+if (parisEl) {
+    let parisDateEl = parisEl.querySelector(".date");
+    let parisTimeEl = parisEl.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
 
-
-parisDateEl.innerHTML = moment().format("MMMM Do, YYYY");
-parisTimeEl.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+    parisDateEl.innerHTML = moment().format("MMMM Do, YYYY");
+    parisTimeEl.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
 }
 
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+        </div>
+    `
+}
+
+updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
